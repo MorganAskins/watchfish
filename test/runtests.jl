427e78143c17979c21c562375d421c39baeb640f
Morgan Askins <maskins@ucdavis.edu>
2019-12-04 13:06:45 -0800

    Documenting some ideas for unit tests. Will write them in later

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,8 @@
 using Test, WatchFish
 
+## Ideas for tests
+# 1. Simple coverage tests. Try to use all functions
+# 2. Benchmarking tests; speed of objective functions
+# 3. Bias / pull for known fits.
+
 @test 1 == 1
