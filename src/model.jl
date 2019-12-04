a28bf9624211d8732b8670aa2dda95dbc54ae8d1
Morgan Askins <maskins@ucdavis.edu>
2019-12-04 12:46:38 -0800

    Swapped from ftol_abs from ftol_rel; ftol_rel is scale invariant, but we are typically using log likelihoods, which are shift invariant, but definetly not scale invariant, in fact, the scale is what determines the total likelihood. Since this is indeed a likelihood, the difference between two points is the likelihood ratio, and can at least be interpreted to some degree as a probability

--- src/model.jl
+++ src/model.jl
@@ -11,24 +11,28 @@ mutable struct Results
 end
 
 ## Generic model functions
-function optimize_model!(m::Model, n::NLogLikelihood; 
+function optimize_model!(m::Model, nll::NLogLikelihood; 
                          lower_bounds=nothing, upper_bounds=nothing)
   function objective(x::Vector, grad::Vector)
     if length(grad)>0
       grad = 2x
     end
-    n.objective(x)
+    nll.objective(x)
   end
-  opt = Opt(:LN_SBPLX, n.numparams)
-  opt.ftol_rel = 1e-4
-  if lower_bounds != nothing
-    opt.lower_bounds = lower_bounds
-  end
-  if upper_bounds != nothing
-    opt.upper_bounds = upper_bounds
-  end
+  opt = Opt(:LN_SBPLX, nll.numparams)
+  # A note here on tolerence. With nlopt one can specify four
+  # types of tolerence for a stopping condition. Either on the
+  # value of the objective function, or on the values of the parameters.
+  # For both of these it can be relative to the scale of each, or
+  # absolute. For simplicity I have forced in tolerance on the objective
+  # function at an absolute scale. This is because I am assuming that
+  # the function is a log-likelihood, and thus has statistical information
+  # in its derivative, but may be shifting by a constant NOT a scale factor.
+  opt.ftol_abs = 1e-6
+  opt.lower_bounds = nll.lower_bounds
+  opt.upper_bounds = nll.upper_bounds
   opt.min_objective = objective
-  p0 = [v.init for v in n.parameters]
+  p0 = [v.init for v in nll.parameters]
   (minf, minx, ret) = optimize!(opt, p0)
   numevals = opt.numevals
   for (i,p) in enumerate(m.params)
