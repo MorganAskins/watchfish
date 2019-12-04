c6a0622887103cef40be4a0a90656eb63c155fa2
Morgan Askins <maskins@ucdavis.edu>
2019-12-04 12:44:10 -0800

    Quadrant based correlation plot filling. In theory this should work better than the initial method since it seeds from the maximum to begin. In practice there seems to still be some issues to be worked out. In particular, correlation_plot2 is actually performing worse than correlation_plot1 for the poisson estimator.

--- src/plotter.jl
+++ src/plotter.jl
@@ -1,4 +1,6 @@
 # Plotting functions
+using Random
+
 function interval_plot(results, name)
   #comp = results.model.component_dict[name]
   ### TODO fix this
@@ -79,7 +81,7 @@ function correlation_plots(results; steps=200)
           nhigh[i], nhigh[j] = a, b
           results.opt.lower_bounds = nlow
           results.opt.upper_bounds = nhigh
-          #p0 = copy(results.min_parameters)
+          #p0 = copy(results.min_parameters) << This is slow
           p0[i], p0[j] = a, b
           minf, minx, ret = optimize!(results.opt, p0)
           if minf == 0
@@ -104,6 +106,106 @@ function correlation_plots(results; steps=200)
         cor = cov / var_a / var_b
         ax[i, j].text(0,0.5, @sprintf("Cor: %0.3f", cor))
         ax[j,i].contourf(A, B, Z, levels=levels)
+        continue
+      end
+      ax[i, i].plot( v.likelihood_x, v.likelihood_y )
+      ax[i, i].set_title(v.name)
+    ax[i,i].set_ylim(0, 1.2)
+    end
+  end
+end
+
+function correlation_plots2(results; steps=100)
+  ## Everything stored in results
+  M = length(results.model.params)
+  fig, ax = subplots(nrows=M, ncols=M)
+
+  levels = exp.(-[Inf,11.83,6.18,2.3,0].^0.5./2)
+
+  for (i, v) in enumerate(results.model.params)
+    for (j, w) in enumerate(results.model.params)
+      if j < M
+        ax[j, i].set_xticklabels([])
+      end
+      if i > 1
+        ax[j, i].set_yticklabels([])
+      end
+      if i > j
+        ax[j, i].axis("off")
+        continue
+      end
+      # profile this parameter
+      nlow = copy(results.lower_bounds)
+      nhigh = copy(results.upper_bounds)
+      p0 = copy(results.min_parameters)
+      nll = []
+      if i != j
+        ## Here we need to go quadrant by quadrant
+        xl = range( v.fit, length=steps, stop=minimum(v.likelihood_x) ) |> collect
+        xr = range( v.fit, length=steps, stop=maximum(v.likelihood_x) ) |> collect
+        yu = range( w.fit, length=steps, stop=maximum(w.likelihood_x) ) |> collect
+        yd = range( w.fit, length=steps, stop=minimum(w.likelihood_x) ) |> collect
+
+        function getz(xset, yset)
+          # We always start at the best fit, so lets reset p0 to best fit
+          coords = []
+          p0 = copy(results.min_parameters)
+          minf0 = copy(results.min_objective)
+          pHold = copy(p0)
+          for y in yset
+            rowz = Array{Float64}(undef, 0)
+            p0 = copy(pHold)
+            reset = true
+            for x in xset
+              nlow[i], nlow[j] = x, y
+              nhigh[i], nhigh[j] = x, y
+              results.opt.lower_bounds = nlow
+              results.opt.upper_bounds = nhigh
+              p0[i], p0[j] = x, y
+              minf, minx, ret = optimize!(results.opt, p0)
+              if minf == 0
+                minf = 10000
+              end
+              push!(rowz, exp(-(minf - minf0)) )
+              if reset
+                reset = false
+                pHold = copy(p0)
+              end
+            end
+            push!(coords, rowz)
+          end
+          return coords
+        end
+        z1 = getz(xr, yu)
+        z2 = getz(xl, yu)
+        z3 = getz(xl, yd)
+        z4 = getz(xr, yd)
+        # Reorder
+        z2 = [reverse(a) for a in z2]
+        z3 = reverse([reverse(a) for a in z3])
+        z4 = reverse(z4)
+        zleft = vcat(z3, z2)
+        zright = vcat(z4, z1)
+        X = vcat(reverse(xl), xr)
+        Y = vcat(reverse(yd), yu)
+        Z = Array([vcat(i, j) for (i,j) in zip(zleft, zright)])
+        Z = hcat(Z...)'
+        Z = Z / maximum(Z)
+
+        ## Given Z, A, B
+        yb = sum(Z, dims=2)
+        ya = transpose( sum(Z, dims=1) )
+        ya = ya / sum(ya)
+        yb = yb / sum(yb)
+
+        mean_a = _mean_comp(X, ya)
+        mean_b = _mean_comp(Y, yb)
+        var_a = sqrt( _variance(X, ya, mean_a) )
+        var_b = sqrt( _variance(Y, yb, mean_b) )
+        cov = _covariance(X, Y, mean_a, mean_b, Z)
+        cor = cov / var_a / var_b
+        ax[i, j].text(0,0.5, @sprintf("Cor: %0.3f", cor))
+        ax[j,i].contourf(X, Y, Z, levels=levels)
         continue
       end
       ax[i, i].plot( v.likelihood_x, v.likelihood_y )
