65c79f209f0898d612b525798c4b659de4f3a33f
Morgan Askins <maskins@ucdavis.edu>
2020-01-30 17:02:49 -0800

    Travis to build docs ... maybe

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,10 @@
 push!(LOAD_PATH, "../src/")
 using Documenter, WatchFish
 
-makedocs(sitename="WatchFish Documentation")
+makedocs(
+  sitename="WatchFish Documentation"
+)
+
+deploydocs(
+  repo = "github.com/MorganAskins/watchfish.git",
+)
