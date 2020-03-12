79102726da23b8e8c67ac9e2b6d69674565f542b
Morgan Askins <maskins@ucdavis.edu>
2020-03-12 09:50:07 -0700

    Updates to examples to include spectral fit changes

--- examples/assets/batroot.py
+++ examples/assets/batroot.py
@@ -3,8 +3,8 @@ import uproot
 import numpy as np
 import os
 
-signal = np.random.multivariate_normal([2.0, 5.0], [[1.0, 0.7], [0.7, 1.0]], size=100000)
-background = np.random.multivariate_normal([3.5, 3.5],[[1.0, -0.7],[-0.7, 1.0]], size=100000)
+signal = np.random.multivariate_normal([2.0, 5.0], [[1.0, 0.7], [0.7, 1.0]], size=1000000)
+background = np.random.multivariate_normal([5.0, 2.0],[[1.0, -0.7],[-0.7, 1.0]], size=1000000)
 sfile = uproot.recreate("assets/signal.root")
 sfile["bat"] = uproot.newtree({"energy":float, "position":float}, title="Batman")
 sfile["bat"].extend({"energy":signal.T[0], "position":signal.T[1]})
